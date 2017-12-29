b1bc011220072c578c16415ac6f647c023d7c5e6
Parker Catalano <catalano.work@gmail.com>
2017-12-29 02:17:09 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,15 +1,20 @@
 class Student
 
-  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url 
+  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url
 
   @@all = []
 
   def initialize(student_hash)
-    
+    student_hash.each do |k, v|
+      self.send("#{k}=", v)
+    end
+    @@all << self
   end
 
   def self.create_from_collection(students_array)
-    
+    students_array.each do |student|
+      Student.new(student)
+    end
   end
 
   def add_student_attributes(attributes_hash)
@@ -17,7 +22,6 @@ class Student
   end
 
   def self.all
-    
+    @@all
   end
 end
-
